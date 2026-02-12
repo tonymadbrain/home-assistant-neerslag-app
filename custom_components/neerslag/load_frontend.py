5498a11f24914f395bd1ab24d5168d9f8b8495f5
Anton Riabov <5266592+tonymadbrain@users.noreply.github.com>
2026-02-12 16:41:50 +0100

    trying to make it work on HA 2026.2.1

--- custom_components/neerslag/load_frontend.py
+++ custom_components/neerslag/load_frontend.py
@@ -1,58 +1,26 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.components.frontend import add_extra_js_url
 from homeassistant.components.http import StaticPathConfig
-from aiohttp import web
 import logging
-import os
-from .const import FRONTEND_SCRIPT_URL, DATA_EXTRA_MODULE_URL
-import time
+from .const import FRONTEND_SCRIPT_URL
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def setup_view(hass: HomeAssistant):
-    timestamp = str(time.time())
-    frontend_script_url_with_parameter = FRONTEND_SCRIPT_URL+"?cache="+timestamp
+    """Register frontend card."""
 
     await hass.http.async_register_static_paths(
         [
             StaticPathConfig(
                 FRONTEND_SCRIPT_URL,
                 hass.config.path("custom_components/neerslag/home-assistant-neerslag-card/neerslag-card.js"),
-                True
+                cache_headers=False,
             )
         ]
     )
-    add_extra_js_url(hass, frontend_script_url_with_parameter , es5=False)
 
-    # Check if Neerslag Card is loaded in frontend, if not: add it again
-    resources = hass.data.get("lovelace", {}).get("resources")
-    if resources:
-        if not resources.loaded:
-            await resources.async_load()
-            resources.loaded = True
+    # Register as ES module
+    add_extra_js_url(hass, FRONTEND_SCRIPT_URL , es5=False)
 
-        frontend_added = False
-        for r in resources.async_items():
-            if r["url"].startswith(FRONTEND_SCRIPT_URL):
-                frontend_added = True
-                continue
-
-        if not frontend_added:
-            if getattr(resources, "async_create_item", None):
-                await resources.async_create_item(
-                    {
-                        "res_type": "module",
-                        "url": FRONTEND_SCRIPT_URL + "?automatically-added" + "&cache=" + timestamp,
-                    }
-                )
-            elif getattr(resources, "data", None) and getattr(
-                resources.data, "append", None
-            ):
-                resources.data.append(
-                    {
-                        "type": "module",
-                        "url": FRONTEND_SCRIPT_URL + "?automatically-added" + "&cache=" + timestamp,
-
-                    }
-                )
+    _LOGGER.debug("Neerslag frontend registered")
